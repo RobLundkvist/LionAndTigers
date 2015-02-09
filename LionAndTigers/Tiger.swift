d0d90521887055e23695fd4616295296c630b71a
Robert Lundkvist <robert@lundkvist.co.uk>
2015-02-09 21:47:11 +0000

    First Release

---
+++ LionAndTigers/Tiger.swift
@@ -0,0 +1,17 @@
+//
+//  Tiger.swift
+//  LionAndTigers
+//
+//  Created by Robert Lundkvist on 09/02/2015.
+//  Copyright (c) 2015 Robert Lundkvist. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named:"")
+}
